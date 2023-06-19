cfb84bd3aa9afa4ff51fa1598f2ee855d87b6943
Fischer <Jessica.Fischer@it.htlhl.at>
2023-06-19 10:46:41 +0200

    programming a pojo which is just like the table in the database

---
+++ src/main/java/at/htlhl/usermanagement/Users.java
@@ -0,0 +1,45 @@
+package at.htlhl.usermanagement;
+
+public class Users {
+    private int id;
+    private String username;
+    private String password;
+    private String email;
+
+    public Users(){}
+
+    public Users(int id, String username, String password, String email) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
