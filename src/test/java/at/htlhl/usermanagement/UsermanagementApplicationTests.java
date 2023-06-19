8d5fc854df170b561143ecc32044721a5aeed488
Fischer <Jessica.Fischer@it.htlhl.at>
2023-06-19 10:40:48 +0200

    initial commit

---
+++ src/test/java/at/htlhl/usermanagement/UsermanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package at.htlhl.usermanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsermanagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
