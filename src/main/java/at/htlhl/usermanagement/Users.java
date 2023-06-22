fb95d335b7116c524d603fdf780f72d7d798633e
Fischer <Jessica.Fischer@it.htlhl.at>
2023-06-22 14:06:49 +0200

    added documentation to Users.java

--- src/main/java/at/htlhl/usermanagement/Users.java
+++ src/main/java/at/htlhl/usermanagement/Users.java
@@ -1,5 +1,16 @@
 package at.htlhl.usermanagement;
 
+/**
+ * @author Fischer
+ * @version 0
+ *
+ * This is the POJO, which has the same parameters as the table in the postgresql-database.
+ * It is used to store the data from the database into a compact object and transfer it easily between the methods.
+ *
+ * <b>Note: </b> The encrypted columns have the datatype String, although in the database itself they are stored as bytea.
+ *          The data gets decrypted in UserRepository.java, but is still encrypted, because the key is owned by the user only.
+ */
+
 public class Users {
     private int id;
     private String username;
