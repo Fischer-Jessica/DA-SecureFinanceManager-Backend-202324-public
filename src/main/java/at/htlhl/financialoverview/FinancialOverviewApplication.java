0be5e5bb745f2ef842e7485a7959f94755e61c16
Fischer <Jessica.Fischer@it.htlhl.at>
2023-06-28 11:46:34 +0200

    initial commit

---
+++ src/main/java/at/htlhl/financialoverview/FinancialOverviewApplication.java
@@ -0,0 +1,13 @@
+package at.htlhl.financialoverview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialOverviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialOverviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/at/htlhl/financialoverview/FinancialOverviewApplicationTests.java
@@ -0,0 +1,13 @@
+package at.htlhl.financialoverview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialOverviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
