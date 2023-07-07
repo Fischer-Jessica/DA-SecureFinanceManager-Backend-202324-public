13863e8ce63810b85dd7bd6f50a9a3fc7ee9f8eb
Fischer <Jessica.Fischer@it.htlhl.at>
2023-07-07 11:47:30 +0200

    added documentation to UserController and UserRepository

--- src/main/java/at/htlhl/financialoverview/controller/UserController.java
+++ src/main/java/at/htlhl/financialoverview/controller/UserController.java
@@ -9,13 +9,27 @@ import io.swagger.v3.oas.annotations.Operation;
 
 import java.util.List;
 
+/**
+ * The UserController class handles the HTTP requests related to user management.
+ *
+ * @author Fischer
+ * @version 1
+ * @since 07.07.2023 (version 1)
+ */
 @RestController
 @CrossOrigin(origins = "*")
 @RequestMapping("financial-overview")
 public class UserController {
+    /** The UserRepository instance for accessing user data. */
     @Autowired
     UserRepository userRepository;
 
+    /**
+     * Returns a list of all users.
+     * This will be restricted or removed in the final product.
+     *
+     * @return A list of all users.
+     */
     @GetMapping("/users")
     @ResponseStatus(HttpStatus.OK)
     @Operation(summary = "returns all users")
@@ -23,6 +37,16 @@ public class UserController {
         return userRepository.getUsers();
     }
 
+    /**
+     * Adds a new user.
+     *
+     * @param username        The username of the new user.
+     * @param password        The password of the new user.
+     * @param eMailAddress    The email address of the new user.
+     * @param firstName       The first name of the new user.
+     * @param lastName        The last name of the new user.
+     * @return The ID of the newly created user.
+     */
     @PostMapping("/users")
     @ResponseStatus(HttpStatus.CREATED)
     @Operation(summary = "add a new user")
@@ -34,6 +58,12 @@ public class UserController {
         return userRepository.addUser(username, password, eMailAddress, firstName, lastName);
     }
 
+    /**
+     * Updates an existing user.
+     *
+     * @param updatedUser     The updated user.
+     * @param loggedInUser    The logged-in user performing the update.
+     */
     @PatchMapping("/users")
     @ResponseStatus(HttpStatus.OK)
     @Operation(summary = "change an existing user")
@@ -41,6 +71,12 @@ public class UserController {
         userRepository.updateUser(updatedUser, loggedInUser);
     }
 
+    /**
+     * Changes the username of an existing user.
+     *
+     * @param updatedUsername The updated username.
+     * @param loggedInUser    The logged-in user performing the update.
+     */
     @PatchMapping("/users/username")
     @ResponseStatus(HttpStatus.OK)
     @Operation(summary = "change the username of an existing user")
@@ -48,6 +84,12 @@ public class UserController {
         userRepository.updateUsername(updatedUsername, loggedInUser);
     }
 
+    /**
+     * Changes the password of an existing user.
+     *
+     * @param updatedPassword The updated password.
+     * @param loggedInUser    The logged-in user performing the update.
+     */
     @PatchMapping("/users/password")
     @ResponseStatus(HttpStatus.OK)
     @Operation(summary = "change the password of an existing user")
@@ -55,6 +97,12 @@ public class UserController {
         userRepository.updatePassword(updatedPassword, loggedInUser);
     }
 
+    /**
+     * Changes the email address of an existing user.
+     *
+     * @param updatedEMailAddress  The updated email address.
+     * @param loggedInUser         The logged-in user performing the update.
+     */
     @PatchMapping("/users/eMailAddress")
     @ResponseStatus(HttpStatus.OK)
     @Operation(summary = "change the e-mail-address of an existing user")
@@ -62,6 +110,12 @@ public class UserController {
         userRepository.updateEMailAddress(updatedEMailAddress, loggedInUser);
     }
 
+    /**
+     * Changes the first name of an existing user.
+     *
+     * @param updatedFirstName The updated first name.
+     * @param loggedInUser     The logged-in user performing the update.
+     */
     @PatchMapping("/users/firstName")
     @ResponseStatus(HttpStatus.OK)
     @Operation(summary = "change the first name of an existing user")
@@ -69,6 +123,12 @@ public class UserController {
         userRepository.updateFirstName(updatedFirstName, loggedInUser);
     }
 
+    /**
+     * Changes the last name of an existing user.
+     *
+     * @param updatedLastName The updated last name.
+     * @param loggedInUser    The logged-in user performing the update.
+     */
     @PatchMapping("/users/lastName")
     @ResponseStatus(HttpStatus.OK)
     @Operation(summary = "change the last name of an existing user")
@@ -76,6 +136,11 @@ public class UserController {
         userRepository.updateLastName(updatedLastName, loggedInUser);
     }
 
+    /**
+     * Deletes a user.
+     *
+     * @param loggedInUser The logged-in user performing the deletion.
+     */
     @DeleteMapping("/users")
     @ResponseStatus(HttpStatus.OK)
     @Operation(summary = "deletes an user")
--- src/main/java/at/htlhl/financialoverview/repository/UserRepository.java
+++ src/main/java/at/htlhl/financialoverview/repository/UserRepository.java
@@ -7,39 +7,103 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+/**
+ * The UserRepository class handles the persistence operations for user data.
+ *
+ * @author Fischer
+ * @version 1
+ * @since 07.07.2023 (version 1)
+ */
 @Repository
 public class UserRepository {
     /** JdbcTemplate which is used in the code below but implementing the template at each usage would be unnecessary */
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
+    /**
+     * Retrieves a list of all users.
+     * This will be restricted or removed in the final product.
+     *
+     * @return A list of User objects representing all users.
+     */
     public List<User> getUsers() {
         return null;
     }
 
+    /**
+     * Adds a new user to the database.
+     *
+     * @param username        The username of the new user.
+     * @param password        The password of the new user.
+     * @param eMailAddress    The email address of the new user.
+     * @param firstName       The first name of the new user.
+     * @param lastName        The last name of the new user.
+     * @return The ID of the newly created user.
+     */
     public int addUser(String username, byte[] password, String eMailAddress, String firstName, String lastName) {
         return 0;
     }
 
-    public void updateUser(User updatedUser, User oldUser) {
+    /**
+     * Updates an existing user with new data.
+     *
+     * @param updatedUser The updated User object.
+     * @param loggedInUser     The original User object to be updated.
+     */
+    public void updateUser(User updatedUser, User loggedInUser) {
 
     }
 
+    /**
+     * Updates the username of an existing user.
+     *
+     * @param username     The updated username.
+     * @param loggedInUser The logged-in user performing the update.
+     */
     public void updateUsername(String username, User loggedInUser) {
     }
 
+    /**
+     * Updates the password of an existing user.
+     *
+     * @param updatedPassword The updated password.
+     * @param loggedInUser    The logged-in user performing the update.
+     */
     public void updatePassword(String updatedPassword, User loggedInUser) {
     }
 
+    /**
+     * Updates the email address of an existing user.
+     *
+     * @param updatedEMailAddress The updated email address.
+     * @param loggedInUser        The logged-in user performing the update.
+     */
     public void updateEMailAddress(String updatedEMailAddress, User loggedInUser) {
     }
 
+    /**
+     * Updates the first name of an existing user.
+     *
+     * @param updatedFirstName The updated first name.
+     * @param loggedInUser     The logged-in user performing the update.
+     */
     public void updateFirstName(String updatedFirstName, User loggedInUser) {
     }
 
+    /**
+     * Updates the last name of an existing user.
+     *
+     * @param updatedLastName The updated last name.
+     * @param loggedInUser    The logged-in user performing the update.
+     */
     public void updateLastName(String updatedLastName, User loggedInUser) {
     }
 
+    /**
+     * Deletes a user from the repository.
+     *
+     * @param loggedInUser The logged-in user performing the deletion.
+     */
     public void deleteUser(User loggedInUser) {
 
     }
