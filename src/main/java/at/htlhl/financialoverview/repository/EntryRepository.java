9585e9bf3fcdcffe228413ab2380c72214db9cb9
Fischer <Jessica.Fischer@it.htlhl.at>
2023-07-24 21:38:45 +0200

    improved the documentation of EntryRepository.java
    improved the names of variables

--- src/main/java/at/htlhl/financialoverview/repository/EntryRepository.java
+++ src/main/java/at/htlhl/financialoverview/repository/EntryRepository.java
@@ -1,78 +1,93 @@
 package at.htlhl.financialoverview.repository;
 
 import at.htlhl.financialoverview.model.Entry;
-import at.htlhl.financialoverview.model.Subcategory;
 import at.htlhl.financialoverview.model.User;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.stereotype.Repository;
 
 import java.sql.*;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Base64;
-import java.util.Date;
 import java.util.List;
 
 /**
  * The EntryRepository class handles the persistence operations for entry data.
+ * It serves as a Spring Data JPA repository for the Entry entity.
+ *
+ * <p>
+ * The EntryRepository serves as an abstraction layer between the EntryController and the underlying data storage, enabling seamless access and manipulation of Entry entities.
+ * </p>
+ *
+ * <p>
+ * This interface should be implemented by a concrete repository class that provides the necessary data access and database operations for Entry entities.
+ * </p>
  *
  * @author Fischer
- * @version 1.2
- * @since 21.07.2023 (version 1.2)
+ * @version 1.3
+ * @since 24.07.2023 (version 1.3)
  */
 @Repository
 public class EntryRepository {
+    /** SQL query to retrieve entries from the 'entries' table in the database for a specific subcategory and user. */
     private static final String SELECT_ENTRIES = "SELECT pk_entry_id, entry_name, entry_description, entry_amount, entry_creation_time, entry_time_of_expense, entry_attachment " +
             "FROM entries " +
             "WHERE fk_user_id = ? AND fk_subcategory_id = ?;";
 
+    /** SQL query to retrieve a specific entry from the 'entries' table in the database for a specific subcategory and user. */
     private static final String SELECT_ENTRY = "SELECT entry_name, entry_description, entry_amount, entry_creation_time, entry_time_of_expense, entry_attachment " +
             "FROM entries " +
             "WHERE pk_entry_id = ? AND fk_user_id = ? AND fk_subcategory_id = ?;";
 
+    /** SQL query to add a new entry to the 'entries' table in the database. */
     private static final String INSERT_ENTRY = "INSERT INTO entries " +
             "(fk_subcategory_id, entry_name, entry_description, entry_amount, entry_creation_time, entry_time_of_expense, entry_attachment, fk_user_id) " +
             "VALUES (?, ?, ?, ?, ?, ?, ?, ?);";
 
+    /** SQL query to update an existing entry in the 'entries' table in the database. */
     private static final String UPDATE_ENTRY = "UPDATE entries " +
             "SET fk_subcategory_id = ?, entry_name = ?, entry_description = ?, entry_amount = ?, entry_time_of_expense = ?, entry_attachment = ? " +
             "WHERE pk_entry_id = ? AND fk_user_id = ?;";
 
+    /** SQL query to update the subcategory ID of an existing entry in the 'entries' table in the database. */
     private static final String UPDATE_SUBCATEGORY_ID = "UPDATE entries " +
             "SET fk_subcategory_id = ? " +
             "WHERE pk_entry_id = ? AND fk_user_id = ?;";
 
+    /** SQL query to update the name of an existing entry in the 'entries' table in the database. */
     private static final String UPDATE_ENTRY_NAME = "UPDATE entries " +
             "SET entry_name = ? " +
             "WHERE pk_entry_id = ? AND fk_user_id = ? AND fk_subcategory_id = ?;";
 
+    /** SQL query to update the description of an existing entry in the 'entries' table in the database. */
     private static final String UPDATE_ENTRY_DESCRIPTION = "UPDATE entries " +
             "SET entry_description = ? " +
             "WHERE pk_entry_id = ? AND fk_user_id = ? AND fk_subcategory_id = ?;";
 
+    /** SQL query to update the amount of an existing entry in the 'entries' table in the database. */
     private static final String UPDATE_ENTRY_AMOUNT = "UPDATE entries " +
             "SET entry_amount = ? " +
             "WHERE pk_entry_id = ? AND fk_user_id = ? AND fk_subcategory_id = ?;";
 
+    /** SQL query to update the time of expense of an existing entry in the 'entries' table in the database. */
     private static final String UPDATE_ENTRY_TIME_OF_EXPENSE = "UPDATE entries " +
             "SET entry_time_of_expense = ? " +
             "WHERE pk_entry_id = ? AND fk_user_id = ? AND fk_subcategory_id = ?;";
 
+    /** SQL query to update the attachment of an existing entry in the 'entries' table in the database. */
     private static final String UPDATE_ENTRY_ATTACHMENT = "UPDATE entries " +
             "SET entry_attachment = ? " +
             "WHERE pk_entry_id = ? AND fk_user_id = ? AND fk_subcategory_id = ?;";
 
+    /** SQL query to delete an entry from the 'entries' table in the database. */
     private static final String DELETE_ENTRY = "DELETE FROM entries " +
             "WHERE pk_entry_id = ? AND fk_user_id = ? AND fk_subcategory_id = ?;";
 
     /**
-     * Retrieves all entries from a specific subcategory.
+     * Retrieves all entries from a specific subcategory for a given user.
      *
      * @param subcategoryId    The ID of the subcategory.
      * @param loggedInUser     The logged-in user.
-     * @return                  A list of entries.
+     * @return A list of entries.
      */
     public List<Entry> getEntries(int subcategoryId, User loggedInUser) {
         if (UserRepository.validateUserCredentials(loggedInUser)) {
@@ -109,12 +124,12 @@ public class EntryRepository {
     }
 
     /**
-     * Retrieves a specific entry from a subcategory.
+     * Retrieves a specific entry from a subcategory for a given user.
      *
-     * @param subcategoryId    The ID of the subcategory.
-     * @param entryId          The ID of the entry.
-     * @param loggedInUser     The logged-in user.
-     * @return                  The entry object.
+     * @param subcategoryId     The ID of the subcategory.
+     * @param entryId           The ID of the entry.
+     * @param loggedInUser      The logged-in user.
+     * @return The entry object.
      */
     public Entry getEntry(int subcategoryId, int entryId, User loggedInUser) {
         if (UserRepository.validateUserCredentials(loggedInUser)) {
@@ -149,17 +164,16 @@ public class EntryRepository {
     }
 
     /**
-     * Adds a new entry to a specific subcategory.
+     * Adds a new entry to a specific subcategory for a given user.
      *
      * @param subcategoryId            The ID of the subcategory.
      * @param entryName                The name of the entry.
      * @param entryDescription         The description of the entry.
      * @param entryAmount              The amount of the entry.
-     * @param entryCreationTime        The creation time of the entry.
      * @param entryTimeOfExpense       The time of expense of the entry.
-     * @param entryAttachment          The amount of the entry (duplicate).
+     * @param entryAttachment          The attachment of the entry.
      * @param loggedInUser             The logged-in user.
-     * @return                         The ID of the added entry.
+     * @return The ID of the added entry.
      */
     public int addEntry(int subcategoryId, String entryName, String entryDescription, String entryAmount, String entryTimeOfExpense, String entryAttachment, User loggedInUser) {
         if (UserRepository.validateUserCredentials(loggedInUser)) {
@@ -194,22 +208,28 @@ public class EntryRepository {
     }
 
     /**
-     * Updates an existing entry in a specific subcategory.
+     * Updates an existing entry in a specific subcategory for a given user.
      *
-     * @param subcategoryId            The ID of the subcategory.
-     * @param loggedInUser             The logged-in user.
+     * @param updatedSubcategoryId              The updated ID of the subcategory.
+     * @param entryId                           The ID of the entry to be updated.
+     * @param updatedEntryName                  The updated name of the entry.
+     * @param updatedEntryDescription           The updated description of the entry.
+     * @param updatedEntryAmount                The updated amount of the entry.
+     * @param updatedEntryTimeOfExpense         The updated time of expense of the entry.
+     * @param updatedEntryAttachment            The updated attachment of the entry.
+     * @param loggedInUser                      The logged-in user.
      */
-    public void updateEntry(int subcategoryId, int entryId, String entryName, String entryDescription, String entryAmount, String entryTimeOfExpense, String entryAttachment, User loggedInUser) {
+    public void updateEntry(int updatedSubcategoryId, int entryId, String updatedEntryName, String updatedEntryDescription, String updatedEntryAmount, String updatedEntryTimeOfExpense, String updatedEntryAttachment, User loggedInUser) {
         if (UserRepository.validateUserCredentials(loggedInUser)) {
             try {
                 Connection conn = UserRepository.jdbcTemplate.getDataSource().getConnection();
                 PreparedStatement ps = conn.prepareStatement(UPDATE_ENTRY);
-                ps.setInt(1, subcategoryId);
-                ps.setBytes(2, Base64.getDecoder().decode(entryName));
-                ps.setBytes(3, Base64.getDecoder().decode(entryDescription));
-                ps.setBytes(4, Base64.getDecoder().decode(entryAmount));
-                ps.setBytes(5, Base64.getDecoder().decode(entryTimeOfExpense));
-                ps.setBytes(6, Base64.getDecoder().decode(entryAttachment));
+                ps.setInt(1, updatedSubcategoryId);
+                ps.setBytes(2, Base64.getDecoder().decode(updatedEntryName));
+                ps.setBytes(3, Base64.getDecoder().decode(updatedEntryDescription));
+                ps.setBytes(4, Base64.getDecoder().decode(updatedEntryAmount));
+                ps.setBytes(5, Base64.getDecoder().decode(updatedEntryTimeOfExpense));
+                ps.setBytes(6, Base64.getDecoder().decode(updatedEntryAttachment));
                 ps.setInt(7, entryId);
                 ps.setInt(8, loggedInUser.getUserId());
                 ps.executeUpdate();
@@ -220,12 +240,19 @@ public class EntryRepository {
         }
     }
 
-    public void updateSubcategoryOfEntry(int subcategoryId, int entryId, User loggedInUser) {
+    /**
+     * Updates the subcategory of an existing entry in the 'entries' table in the database.
+     *
+     * @param updatedSubcategoryId      The updated ID of the subcategory.
+     * @param entryId                   The ID of the entry to be updated.
+     * @param loggedInUser              The logged-in user.
+     */
+    public void updateSubcategoryOfEntry(int updatedSubcategoryId, int entryId, User loggedInUser) {
         if (UserRepository.validateUserCredentials(loggedInUser)) {
             try {
                 Connection conn = UserRepository.jdbcTemplate.getDataSource().getConnection();
                 PreparedStatement ps = conn.prepareStatement(UPDATE_SUBCATEGORY_ID);
-                ps.setInt(1, subcategoryId);
+                ps.setInt(1, updatedSubcategoryId);
                 ps.setInt(2, entryId);
                 ps.setInt(3, loggedInUser.getUserId());
                 ps.executeUpdate();
@@ -237,9 +264,10 @@ public class EntryRepository {
     }
 
     /**
-     * Updates the name of an existing entry in a specific subcategory.
+     * Updates the name of an existing entry in a specific subcategory for a given user.
      *
      * @param subcategoryId            The ID of the subcategory.
+     * @param entryId                  The ID of the entry to be updated.
      * @param updatedEntryName         The updated name of the entry.
      * @param loggedInUser             The logged-in user.
      */
@@ -261,11 +289,12 @@ public class EntryRepository {
     }
 
     /**
-     * Updates the description of an existing entry in a specific subcategory.
+     * Updates the description of an existing entry in a specific subcategory for a given user.
      *
-     * @param subcategoryId            The ID of the subcategory.
-     * @param updatedEntryDescription  The updated description of the entry.
-     * @param loggedInUser             The logged-in user.
+     * @param subcategoryId                 The ID of the subcategory.
+     * @param entryId                       The ID of the entry to be updated.
+     * @param updatedEntryDescription       The updated description of the entry.
+     * @param loggedInUser                  The logged-in user.
      */
     public void updateEntryDescription(int subcategoryId, int entryId, String updatedEntryDescription, User loggedInUser) {
         if (UserRepository.validateUserCredentials(loggedInUser)) {
@@ -285,9 +314,10 @@ public class EntryRepository {
     }
 
     /**
-     * Updates the amount of an existing entry in a specific subcategory.
+     * Updates the amount of an existing entry in a specific subcategory for a given user.
      *
      * @param subcategoryId            The ID of the subcategory.
+     * @param entryId                  The ID of the entry to be updated.
      * @param updatedEntryAmount       The updated amount of the entry.
      * @param loggedInUser             The logged-in user.
      */
@@ -309,18 +339,19 @@ public class EntryRepository {
     }
 
     /**
-     * Updates the time of expense of an existing entry in a specific subcategory.
+     * Updates the time of expense of an existing entry in a specific subcategory for a given user.
      *
-     * @param subcategoryId            The ID of the subcategory.
-     * @param entryTimeOfExpense       The updated time of expense of the entry.
-     * @param loggedInUser             The logged-in user.
+     * @param subcategoryId                     The ID of the subcategory.
+     * @param entryId                           The ID of the entry to be updated.
+     * @param updatedEntryTimeOfExpense         The updated time of expense of the entry.
+     * @param loggedInUser                      The logged-in user.
      */
-    public void updateEntryTimeOfExpense(int subcategoryId, int entryId, String entryTimeOfExpense, User loggedInUser) {
+    public void updateEntryTimeOfExpense(int subcategoryId, int entryId, String updatedEntryTimeOfExpense, User loggedInUser) {
         if (UserRepository.validateUserCredentials(loggedInUser)) {
             try {
                 Connection conn = UserRepository.jdbcTemplate.getDataSource().getConnection();
                 PreparedStatement ps = conn.prepareStatement(UPDATE_ENTRY_TIME_OF_EXPENSE);
-                ps.setBytes(1, Base64.getDecoder().decode(entryTimeOfExpense));
+                ps.setBytes(1, Base64.getDecoder().decode(updatedEntryTimeOfExpense));
                 ps.setInt(2, entryId);
                 ps.setInt(3, loggedInUser.getUserId());
                 ps.setInt(4, subcategoryId);
@@ -333,11 +364,12 @@ public class EntryRepository {
     }
 
     /**
-     * Updates the attachment of an existing entry in a specific subcategory.
+     * Updates the attachment of an existing entry in a specific subcategory for a given user.
      *
-     * @param subcategoryId            The ID of the subcategory.
-     * @param updatedEntryAttachment    The updated attachment of the entry.
-     * @param loggedInUser             The logged-in user.
+     * @param subcategoryId                 The ID of the subcategory.
+     * @param entryId                       The ID of the entry to be updated.
+     * @param updatedEntryAttachment        The updated attachment of the entry.
+     * @param loggedInUser                  The logged-in user.
      */
     public void updateEntryAttachment(int subcategoryId, int entryId, String updatedEntryAttachment, User loggedInUser) {
         if (UserRepository.validateUserCredentials(loggedInUser)) {
@@ -357,7 +389,7 @@ public class EntryRepository {
     }
 
     /**
-     * Deletes an entry from a specific subcategory.
+     * Deletes an entry from a specific subcategory for a given user.
      *
      * @param subcategoryId            The ID of the subcategory.
      * @param entryId                  The ID of the entry to be deleted.
