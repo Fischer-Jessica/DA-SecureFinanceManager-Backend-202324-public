8924b6f7c3a429f39a1cc11b2decb82caafd7257
Fischer <Jessica.Fischer@it.htlhl.at>
2023-07-24 20:39:15 +0200

    improved the documentation of Entry.java
    improved the names of variables

--- src/main/java/at/htlhl/financialoverview/model/Entry.java
+++ src/main/java/at/htlhl/financialoverview/model/Entry.java
@@ -19,44 +19,48 @@ import java.util.List;
  * </p>
  *
  * @author Fischer
- * @version 1.3
- * @since 21.07.2023 (version 1.3)
+ * @version 1.4
+ * @since 24.07.2023 (version 1.4)
  *
  * @see Category this class (Category) for the explanations of the annotations
  */
+
 @Entity
 @Table(name = "entries")
 @SecondaryTable(name = "subcategories", pkJoinColumns = @PrimaryKeyJoinColumn(name = "fk_subcategory_id"))
 @SecondaryTable(name = "users", pkJoinColumns = @PrimaryKeyJoinColumn(name = "fk_user_id"))
 @SecondaryTable(name = "labels", pkJoinColumns = @PrimaryKeyJoinColumn(name = "fk_label_id"))
 public class Entry {
-    /** the id of the entry */
+    /**
+     * The unique identifier for this Entry.
+     * It is generated automatically by the database.
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "pk_entry_id")
     private int entryId;
 
-    /** the name of the entry */
+    /** The name of the entry. */
     @Column(name = "entry_name")
     private String entryName;
 
-    /** the description of the entry */
+    /** The description of the entry. */
     @Column(name = "entry_description")
     private String entryDescription;
 
-    /** the amount of the entry */
+    /** The amount of the entry. */
     @Column(name = "entry_amount")
     private String entryAmount;
 
-    /** the creation time of the entry */
+    /** The creation time of the entry. */
     @Column(name = "entry_creation_time")
     private String entryCreationTime;
 
-    /** the time of expense for the entry */
+    /** The time of expense for the entry. */
     @Column(name = "entry_time_of_expense")
     private String entryTimeOfExpense;
 
-    /** the attachment of the entry */
+    /** The attachment of the entry. */
     @Column(name = "entry_attachment")
     private String entryAttachment;
 
@@ -68,8 +72,9 @@ public class Entry {
     @JoinColumn(name = "fk_subcategory_id", referencedColumnName = "pk_subcategory_id", table = "subcategories", insertable=false, updatable=false)
     private Subcategory subcategory;
 
+    /** The ID of the subcategory associated with the Entry. */
     @Column(name = "fk_subcategory_id")
-    private int subcategoryId;
+    private int entrySubcategoryId;
 
 
     /**
@@ -80,8 +85,9 @@ public class Entry {
     @JoinColumn(name = "fk_user_id", referencedColumnName = "pk_user_id", table = "users", insertable=false, updatable=false)
     private User user;
 
+    /** The ID of the user associated with the Entry. */
     @Column(name = "fk_user_id")
-    private int userId;
+    private int entryUserId;
 
     /**
      * Many-to-many relationship with Label.
@@ -95,9 +101,25 @@ public class Entry {
     )
     private List<Label> labels;
 
+    /**
+     * Default constructor.
+     */
     public Entry() {}
 
-    public Entry(int entryId, String entryName, String entryDescription, String entryAmount, String entryCreationTime, String entryTimeOfExpense, String entryAttachment, int subcategoryId, int userId) {
+    /**
+     * Constructs a new Entry object with the specified properties.
+     *
+     * @param entryId               The ID of the entry.
+     * @param entryName             The name of the entry.
+     * @param entryDescription      The description of the entry.
+     * @param entryAmount           The amount of the entry.
+     * @param entryCreationTime     The creation time of the entry.
+     * @param entryTimeOfExpense    The time of expense for the entry.
+     * @param entryAttachment       The attachment of the entry.
+     * @param entrySubcategoryId    The ID of the associated subcategory.
+     * @param entryUserId           The ID of the associated user.
+     */
+    public Entry(int entryId, String entryName, String entryDescription, String entryAmount, String entryCreationTime, String entryTimeOfExpense, String entryAttachment, int entrySubcategoryId, int entryUserId) {
         this.entryId = entryId;
         this.entryName = entryName;
         this.entryDescription = entryDescription;
@@ -107,39 +129,84 @@ public class Entry {
         this.entryAttachment = entryAttachment;
     }
 
+    /**
+     * Returns the ID of the entry.
+     *
+     * @return The ID of the entry.
+     */
     public int getEntryId() {
         return entryId;
     }
 
+    /**
+     * Returns the name of the entry.
+     *
+     * @return The name of the entry.
+     */
     public String getEntryName() {
         return entryName;
     }
 
+    /**
+     * Returns the description of the entry.
+     *
+     * @return The description of the entry.
+     */
     public String getEntryDescription() {
         return entryDescription;
     }
 
+    /**
+     * Returns the amount of the entry.
+     *
+     * @return The amount of the entry.
+     */
     public String getEntryAmount() {
         return entryAmount;
     }
 
+    /**
+     * Returns the creation time of the entry.
+     *
+     * @return The creation time of the entry.
+     */
     public String getEntryCreationTime() {
         return entryCreationTime;
     }
 
+    /**
+     * Returns the time of expense for the entry.
+     *
+     * @return The time of expense for the entry.
+     */
     public String getEntryTimeOfExpense() {
         return entryTimeOfExpense;
     }
 
+    /**
+     * Returns the attachment of the entry.
+     *
+     * @return The attachment of the entry.
+     */
     public String getEntryAttachment() {
         return entryAttachment;
     }
 
-    public int getSubcategoryId() {
-        return subcategoryId;
+    /**
+     * Returns the ID of the associated subcategory.
+     *
+     * @return The ID of the associated subcategory.
+     */
+    public int getEntrySubcategoryId() {
+        return entrySubcategoryId;
     }
 
-    public int getUserId() {
-        return userId;
+    /**
+     * Returns the ID of the associated user.
+     *
+     * @return The ID of the associated user.
+     */
+    public int getEntryUserId() {
+        return entryUserId;
     }
 }
