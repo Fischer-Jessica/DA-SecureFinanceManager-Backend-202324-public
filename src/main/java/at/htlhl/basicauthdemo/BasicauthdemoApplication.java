ebe9a33ff1ed476c040d560b85a9b4f04fbb71db
Fischer <Jessica.Fischer@it.htlhl.at>
2023-10-13 15:34:07 +0200

    test-program from WIH

---
+++ src/main/java/at/htlhl/basicauthdemo/BasicauthdemoApplication.java
@@ -0,0 +1,13 @@
+package at.htlhl.basicauthdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicauthdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasicauthdemoApplication.class, args);
+    }
+
+}
