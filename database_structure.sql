a4c79b917a29d1cb0b568e4277d6e2265aa82107
Fischer <Jessica.Fischer@it.htlhl.at>
2023-11-09 10:50:45 +0100

    the third version of the database design

--- database_structure.sql
+++ database_structure.sql
@@ -6,14 +6,14 @@ CREATE EXTENSION IF NOT EXISTS pgcrypto;
 
 CREATE TABLE colours (
 	pk_colour_id SERIAL PRIMARY KEY,
-	colour_name TEXT,
-	colour_code bytea
+	colour_name TEXT NOT NULL,
+	colour_code bytea NOT NULL
 );
 
 CREATE TABLE users (
 	pk_user_id SERIAL PRIMARY KEY,
-	username TEXT,
-	password bytea,
+	username TEXT UNIQUE NOT NULL,
+	password bytea NOT NULL,
 	email_address TEXT UNIQUE,
 	first_name TEXT,
 	last_name TEXT
@@ -21,33 +21,33 @@ CREATE TABLE users (
 
 CREATE TABLE categories (
 	pk_category_id SERIAL PRIMARY KEY,
-	category_name bytea,
+	category_name bytea NOT NULL,
 	category_description bytea,
-	fk_category_colour_id INT,
-	fk_user_id INT,
-	FOREIGN KEY (fk_user_id) REFERENCES users(pk_user_id),
+	fk_category_colour_id INT NOT NULL,
+	fk_user_id INT NOT NULL,
+	FOREIGN KEY (fk_user_id) REFERENCES users(pk_user_id) ON DELETE CASCADE,
 	FOREIGN KEY (fk_category_colour_id) REFERENCES colours(pk_colour_id)
 );
 
 CREATE TABLE subcategories (
 	pk_subcategory_id SERIAL PRIMARY KEY,
-	fk_category_id INT,
-	subcategory_name bytea,
+	fk_category_id INT NOT NULL,
+	subcategory_name bytea NOT NULL,
 	subcategory_description bytea,
-	fk_subcategory_colour_id INT,
-	fk_user_id INT,
-	FOREIGN KEY (fk_user_id) REFERENCES users(pk_user_id),
-	FOREIGN KEY (fk_category_id) REFERENCES categories(pk_category_id),
+	fk_subcategory_colour_id INT NOT NULL,
+	fk_user_id INT NOT NULL,
+	FOREIGN KEY (fk_user_id) REFERENCES users(pk_user_id) ON DELETE CASCADE,
+	FOREIGN KEY (fk_category_id) REFERENCES categories(pk_category_id) ON DELETE CASCADE,
 	FOREIGN KEY (fk_subcategory_colour_id) REFERENCES colours(pk_colour_id)
 );
 
 CREATE TABLE labels (
 	pk_label_id SERIAL PRIMARY KEY,
-	label_name bytea,
+	label_name bytea NOT NULL,
 	label_description bytea,
-	fk_label_colour_id INT,
-	fk_user_id INT,
-	FOREIGN KEY (fk_user_id) REFERENCES users(pk_user_id),
+	fk_label_colour_id INT NOT NULL,
+	fk_user_id INT NOT NULL,
+	FOREIGN KEY (fk_user_id) REFERENCES users(pk_user_id) ON DELETE CASCADE,
 	FOREIGN KEY (fk_label_colour_id) REFERENCES colours(pk_colour_id)
 );
 
@@ -55,24 +55,24 @@ CREATE TABLE entries (
 	pk_entry_id SERIAL PRIMARY KEY,
 	entry_name bytea,
 	entry_description bytea,
-	entry_amount bytea,
-	entry_creation_time bytea,
-	entry_time_of_transaction bytea,
+	entry_amount bytea NOT NULL,
+	entry_creation_time bytea NOT NULL,
+	entry_time_of_transaction bytea NOT NULL,
 	entry_attachment bytea,
-	fk_subcategory_id INT,
-	fk_user_id INT,
-	FOREIGN KEY (fk_user_id) REFERENCES users(pk_user_id),
-	FOREIGN KEY (fk_subcategory_id) REFERENCES subcategories(pk_subcategory_id)
+	fk_subcategory_id INT NOT NULL,
+	fk_user_id INT NOT NULL,
+	FOREIGN KEY (fk_user_id) REFERENCES users(pk_user_id) ON DELETE CASCADE,
+	FOREIGN KEY (fk_subcategory_id) REFERENCES subcategories(pk_subcategory_id) ON DELETE CASCADE
 );
 
 CREATE TABLE entry_labels (
 	pk_entry_label_id SERIAL PRIMARY KEY,
-	fk_entry_id INT,
-	fk_label_id INT,
-	fk_user_id INT,
-	FOREIGN KEY (fk_user_id) REFERENCES users(pk_user_id),
-	FOREIGN KEY (fk_entry_id) REFERENCES entries(pk_entry_id),
-	FOREIGN KEY (fk_label_id) REFERENCES labels(pk_label_id)
+	fk_entry_id INT NOT NULL,
+	fk_label_id INT NOT NULL,
+	fk_user_id INT NOT NULL,
+	FOREIGN KEY (fk_user_id) REFERENCES users(pk_user_id) ON DELETE CASCADE,
+	FOREIGN KEY (fk_entry_id) REFERENCES entries(pk_entry_id) ON DELETE CASCADE,
+	FOREIGN KEY (fk_label_id) REFERENCES labels(pk_label_id) ON DELETE CASCADE
 );
 
 CREATE UNIQUE INDEX entry_labels_unique_idx ON entry_labels (fk_entry_id, fk_label_id, fk_user_id);
--- database_user_permissions.sql
+++ database_user_permissions.sql
@@ -1,15 +1,32 @@
-REVOKE CREATE ON SCHEMA public FROM PUBLIC;
+-- Erstellen Sie den Benutzer "backend" mit einem sicheren Passwort
+CREATE ROLE backend WITH PASSWORD 'SECURE_PASSWORD' LOGIN;
 
-CREATE ROLE backend WITH PASSWORD 'SECURE_PASSWORD';
-
+-- Verbindung zur Datenbank gewähren
 GRANT CONNECT ON DATABASE secure_finance_manager TO backend;
 
-GRANT USAGE ON SCHEMA public TO backend;
+-- Berechtigungen für Tabellen erteilen
+GRANT SELECT ON public.colours TO backend;
 
-GRANT SELECT, INSERT, UPDATE ON ALL TABLES IN SCHEMA public TO backend;
+GRANT SELECT, INSERT, UPDATE, DELETE ON public.users TO backend;
 
-GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA public TO backend;
+GRANT USAGE ON public.users_pk_user_id_seq TO backend;
 
-ALTER ROLE backend WITH LOGIN;
+GRANT SELECT, INSERT, UPDATE, DELETE, REFERENCES ON public.categories TO backend;
 
-ALTER USER backend WITH SUPERUSER;
+GRANT USAGE ON public.categories_pk_category_id_seq TO backend;
+
+GRANT SELECT, INSERT, UPDATE, DELETE, REFERENCES ON public.subcategories TO backend;
+
+GRANT USAGE ON public.subcategories_pk_subcategory_id_seq TO backend;
+
+GRANT SELECT, INSERT, UPDATE, DELETE, REFERENCES ON public.entries TO backend;
+
+GRANT USAGE ON public.entries_pk_entry_id_seq TO backend;
+
+GRANT SELECT, INSERT, UPDATE, DELETE, REFERENCES ON public.labels TO backend;
+
+GRANT USAGE ON public.labels_pk_label_id_seq TO backend;
+
+GRANT SELECT, INSERT, UPDATE, DELETE, REFERENCES ON public.entry_labels TO backend;
+
+GRANT USAGE ON public.entry_labels_pk_entry_label_id_seq TO backend;
